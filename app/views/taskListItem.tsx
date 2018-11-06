b75aa178c260d180b3977827325443be9f4aeff8
Jack <jack9966qk@gmail.com>
2018-11-06 19:22:37 +0800

    fix progress bar display for ongoing bittorrent tasks

--- app/views/taskListItem.tsx
+++ app/views/taskListItem.tsx
@@ -235,7 +235,7 @@ class TaskListItem extends React.Component<TaskListItemProps, TaskListItemState>
                     }
                 </div>
                 
-                { (status === "active" && isHttp(task)) ? progressBar : "" }
+                { (status === "active" && !downloadComplete(task)) ? progressBar : "" }
             </Paper>
         )
     }
